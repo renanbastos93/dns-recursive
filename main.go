ec56bb9cba8eea1169f48a592580a0961215ab8c
renanbastos93 <renanbastos.tec@gmail.com>
2019-10-10 00:10:13 -0300

    feat: experimental miek dns

--- main.go
+++ main.go
@@ -1,42 +1,61 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/domainr/dnsr"
+    "github.com/miekg/dns"
+    "net"
+    "log"
+    "fmt"
+    "time"
 )
 
-type answer struct {
-	Question string
-	Type     string
-	Value    string
+type Resolver struct {
+    ok bool
+	timeout time.Duration
 }
 
-type iDNSRecursive struct {
-	ok      bool
-	answers []answer
+
+func recursiveDNS(question string, qType uint16) {
+
+    config, _ := dns.ClientConfigFromFile("/etc/resolv.conf")
+    c := new(dns.Client)
+
+    m := new(dns.Msg)
+    m.SetQuestion(dns.Fqdn(question), dns.TypeA)
+    m.RecursionDesired = true
+
+    r, _, err := c.Exchange(m, net.JoinHostPort(config.Servers[0], config.Port))
+
+    if r == nil {
+        log.Fatalf("*** error: %s\n", err.Error())
+    }
+
+    if dns.RcodeNameError == r.Rcode {
+        log.Fatalf(" *** error: NXDOMAIN")
+    }
+
+    fmt.Println(r)
 }
 
-var iDNSr iDNSRecursive
-var iDNSAnswer answer
-
-func iDNSrecursive(question string, qtype string) {
-	r := dnsr.New(1000)
-	resolved := r.Resolve(question, qtype)
-	for _, rr := range resolved {
-		if rr.Type == "A" || rr.Type == "AAAA" {
-			iDNSr.ok = true
-			iDNSAnswer.Question = question
-			iDNSAnswer.Type = rr.Type
-			iDNSAnswer.Value = rr.Value
-			iDNSr.answers = append(iDNSr.answers, iDNSAnswer)
-			break
-		}
-	}
-}
 
 func main() {
-	iDNSrecursive("www.renanbastos.com.br", "A")
-	iDNSrecursive("www.google.com", "A")
-	fmt.Println(iDNSr) // {true [{www.renanbastos.com.br A 191.6.198.85} {www.google.com A 172.217.29.164}]}
+    
+    // apex := "www.renanbastos.com.br"
+    apex := "ssh.sbh.eng.br"
+    // apex := "sbh.eng.br"
+    // apex := "sdasda.op" 
+
+    recursiveDNS(apex, dns.TypeA)
+    
+
+    // if r.Rcode != dns.RcodeSuccess {
+    //         log.Fatalf("invalid answer name after %s query for %s\n", r.Rcode,apex)
+    // }
+    // // Stuff must be in the answer section
+    // if len(r.Answer) < 1 {
+    //     fmt.Println(r.Answer)
+    // }
+    // for _, a := range r.Answer {
+    //         fmt.Printf("%v\n", a)
+    // }
 }
+
